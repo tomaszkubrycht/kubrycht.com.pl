81db56fb666f87ef6645004e38d132d9ea0f061a
TomaszKubrycht <tomasz.kubrycht@gmail.com>
2017-10-08 14:56:45 +0200

    Add project files.

---
+++ kubrycht.com.pl/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kubrycht.com.pl.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
